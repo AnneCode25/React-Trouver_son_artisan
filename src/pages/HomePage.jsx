8860c6352c87aeb008ff401337d0e4cd16f972ec
AnneVi <villette55@gmail.com>
2024-07-30 19:21:31 +0200

    création structure + composants

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Layout from '../components/layout/Layout'
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <h1>Bienvenue sur notre site</h1>
+      <p>Contenu spécifique à la page accueil</p>
+    </Layout>
+  );
+};
+
+export default HomePage;
