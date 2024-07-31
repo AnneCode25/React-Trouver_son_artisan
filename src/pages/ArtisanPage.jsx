5b178c66ace551aff264f612377981746c5d1852
AnneVi <villette55@gmail.com>
2024-07-31 16:29:34 +0200

    initialisation router

---
+++ src/pages/ArtisanPage.jsx
@@ -0,0 +1,12 @@
+import Layout from '../components/layout/Layout'
+
+const ArtisanPage = () => {
+  return (
+    <Layout>
+      <h1>Page artisan</h1>
+      <p>Contenu spécifique à la page accueil</p>
+    </Layout>
+  );
+};
+
+export default ArtisanPage;
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,17 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Page non trouvée</h1>
+      <p>
+        Nous sommes désolés mais la page que vous avez demandée n&apos;existe pas.
+      </p>
+      <p>
+        Veuillez retourner à la page d&apos;accueil ou essayer une autre recherche.
+      </p>
+      <p>Merci de votre compréhension.</p>
+      <a href="/">Retour à l&apos;accueil</a>
+    </div>
+  );
+};
+
+export default NotFoundPage;
