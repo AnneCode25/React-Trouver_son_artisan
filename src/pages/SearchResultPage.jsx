a587bcb2f60a6990d31f1b6573c977f0633f20c1
AnneVi <villette55@gmail.com>
2024-08-10 23:24:16 +0200

    suite structure fonction recherche

--- src/pages/SearchResultPage.jsx
+++ src/pages/SearchResultPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import StarRating from '../components/commons/StarRating'
 
 // Importez vos données JSON ici
 import data from '../data/datas.json';
@@ -7,11 +8,10 @@ import data from '../data/datas.json';
 const SearchResultPage = () => {
   const [searchResults, setSearchResults] = useState([]);
   const location = useLocation();
+  const searchTerm = location.state?.searchTerm || '';
 
   useEffect(() => {
-    const searchTerm = location.state?.searchTerm || '';
-    
-    if (searchTerm) {
+      if (searchTerm) {
       const results = data.filter(item =>
         item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
         item.specialty.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -21,22 +21,35 @@ const SearchResultPage = () => {
     } else {
       setSearchResults([]);
     }
-  }, [location.state]);
+  }, [searchTerm]);
 
   return (
     <div className="container mt-4">
-      <h2>Résultats de recherche</h2>
+      <h1 className="mb-4">Recherche</h1>
+      <h2 className="h4 mb-3">Recherche par mot clé</h2>
+      <p className="mb-3">Résultats pour : <strong>{searchTerm}</strong></p>
+      <p className="mb-4">{searchResults.length} résultat(s) trouvé(s)</p>
+
       {searchResults.length === 0 ? (
         <p>Aucun résultat trouvé.</p>
       ) : (
-        <ul className="list-group">
+        <div className="row">
           {searchResults.map((item, index) => (
-            <li key={index} className="list-group-item">
-              <strong>{item.name}</strong> - {item.specialty} à {item.location}
-            </li>
+            <div key={index} className="col-12 mb-3">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{item.name}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">{item.specialty}</h6>
+                  <p className="card-text">{item.location}</p>
+                  <StarRating rating={parseFloat(item.note)} />
+                  </div>
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
+{/* `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`. */}
+      <Link to="/" className="btn btn-primary mt-4">Retour à l&apos;accueil</Link>
     </div>
   );
 };
