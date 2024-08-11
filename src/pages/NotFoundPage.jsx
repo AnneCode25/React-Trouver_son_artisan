4e9d7ff2acc914a6d0819bb0ecfca5590a0a0bbf
AnneVi <villette55@gmail.com>
2024-08-11 16:17:16 +0200

    structure page error

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,19 +1,22 @@
+import { Link } from 'react-router-dom'
 import Layout from "../components/layout/Layout";
+import error from '../assets/images/error404.png'
 
 const NotFoundPage = () => {
   return (
-        <Layout>
-    <div>
-      <h1>Page non trouvée</h1>
-      <p>
-        Nous sommes désolés mais la page que vous avez demandée n&apos;existe pas.
-      </p>
-      <p>
-        Veuillez retourner à la page d&apos;accueil ou essayer une autre recherche.
-      </p>
-      <p>Merci de votre compréhension.</p>
-      <a href="/">Retour à l&apos;accueil</a>
-    </div>
+    <Layout>
+      <div className="d-flex flex-column align-items-center justify-content-center min-vh-100 text-center">
+        <h2 className="display-1 fw-bold">Error 404</h2>
+        <img 
+          src={error}
+          alt="Erreur 404"
+          className="img-fluid mb-4"
+          style={{ maxWidth: "100%", width: "50em", height: "auto" }}        />
+        <p className="lead mb-4">Page non trouvée</p>
+        <button>
+          <Link to="/">Retour à l&apos;accueil</Link>
+        </button>
+      </div>
     </Layout>
   );
 };
